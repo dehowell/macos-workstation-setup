28a948ae59fb3c043f37226f1bc6960ea91bea22
David Howell <dehowell@gmail.com>
2020-12-12 08:57:55 -0500

    Add defer by one day plugin, like Things

--- macos-keyboard-shortcuts.sh
+++ macos-keyboard-shortcuts.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-defaults write com.omnigroup.OmniFocus3 NSUserKeyEquivalents -dict-add "Open Project Note" -string '@$p'
+function addOmniFocusShortcut() {
+    defaults write com.omnigroup.OmniFocus3 NSUserKeyEquivalents -dict-add "$1" -string $2
+}
+
+addOmniFocusShortcut "Open Project Note" '@$p'
+addOmniFocusShortcut "Defer One Day" '^]'
